5245ebd89c983e185130fd958a5f6744d03ee35e
Derex <Derex@garyzhaodeimac-2.local>
2016-03-31 18:30:07 +0800

    Complete CalculatorBrain

--- NewCalculator/CaculatorBrain.swift
+++ NewCalculator/CaculatorBrain.swift
@@ -22,7 +22,37 @@ class CaculatorBrain {
         knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
         knownOps["+"] = Op.BinaryOperation("+", *)
     }
-    
+    func evaluate() -> Double? {
+        let (result, reminder) = evaluate(opStack)
+        print("remainingOps: \(reminder)")
+        return result
+        
+    }
+    private func evaluate(ops:[Op]) -> (result:Double?, remaingsOps:[Op]) {
+        if !ops.isEmpty {
+            var remaingsOps = ops
+            let op = remaingsOps.removeLast()
+            switch op {
+            case .operand(let operand):
+                return (operand, remaingsOps)
+            case .UnaryOperation(_, let operation):
+                let evluatioinOperation = evaluate(remaingsOps)
+                if let operand = evluatioinOperation.result {
+                return (operation(operand), evluatioinOperation.remaingsOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let evluationOperation = evaluate(remaingsOps)
+                if let operand1 = evluationOperation.result{
+                    let evluationOperation1 = evaluate(evluationOperation.remaingsOps)
+                    if let operand2 = evluationOperation1.result {
+                        return (operation(operand1,operand2), evluationOperation1.remaingsOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
     private enum Op {
         case operand(Double)
         case UnaryOperation(String, Double -> Double)
@@ -32,7 +62,9 @@ class CaculatorBrain {
     func pushOperand(operand: Double){
         opStack.append(Op.operand(operand))
     }
-//    func performOperation(symbol: String) {
-//        
-//    }
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+        opStack.append(operation)
+        }
+    }
 }
